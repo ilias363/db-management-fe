339304b650a34b7d533f2be12cc624c1b507cb8a
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-08-07 22:22:22 +0100

    change stat card icon size

--- components/common/stats-card.tsx
+++ components/common/stats-card.tsx
@@ -20,7 +20,7 @@ export function StatsCard({
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-sm font-medium">{title}</CardTitle>
-        <Icon className={`h-4 w-4 ${iconColor}`} />
+        <Icon className={`h-5 w-5 ${iconColor}`} />
       </CardHeader>
       <CardContent>
         <div className="text-2xl font-bold">{value ?? "--"}</div>
