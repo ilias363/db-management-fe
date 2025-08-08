1e0bbcbed5c1c09bb6117f47466a29ad0bddaf39
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-08-08 16:37:14 +0100

    update schemas, tables and views response types

--- lib/types/metadata.ts
+++ lib/types/metadata.ts
@@ -4,11 +4,11 @@ import { ApiResponse } from './index';
 export * from './database';
 
 export type SchemaResponse = ApiResponse<import('./database').SchemaMetadataDto>;
-export type SchemasResponse = ApiResponse<import('./database').SchemaMetadataDto[]>;
+export type SchemasResponse = ApiResponse<Omit<import('./database').SchemaMetadataDto, "tables" | "views">[]>;
 export type TableResponse = ApiResponse<import('./database').TableMetadataDto>;
-export type TablesResponse = ApiResponse<import('./database').TableMetadataDto[]>;
+export type TablesResponse = ApiResponse<Omit<import('./database').TableMetadataDto, "schema" | "columns" | "indexes">[]>;
 export type ViewResponse = ApiResponse<import('./database').ViewMetadataDto>;
-export type ViewsResponse = ApiResponse<import('./database').ViewMetadataDto[]>;
+export type ViewsResponse = ApiResponse<Omit<import('./database').ViewMetadataDto, "schema" | "columns">[]>;
 export type ColumnResponse = ApiResponse<import('./database').BaseColumnMetadataDto>;
 export type ColumnsResponse = ApiResponse<import('./database').BaseColumnMetadataDto[]>;
 export type IndexResponse = ApiResponse<import('./database').IndexMetadataDto>;
