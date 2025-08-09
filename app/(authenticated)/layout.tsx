66c59faf20204b8a7114da12006522fac6ecdc70
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-08-09 12:16:13 +0100

    setup react query

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,10 +1,13 @@
 import { AuthProvider } from "@/components/auth/auth-provider";
 import MainLayout from "@/components/layout/main-layout";
+import QueryProvider from "@/components/react-query/query-provider";
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
-    <AuthProvider>
-      <MainLayout>{children}</MainLayout>
-    </AuthProvider>
+    <QueryProvider>
+      <AuthProvider>
+        <MainLayout>{children}</MainLayout>
+      </AuthProvider>
+    </QueryProvider>
   );
 }
