4e6d0e2ecc38869a28075026a6f688c7b6b93c49
Ouaddaf Ilias <ouaddafilyas@gmail.com>
2025-08-11 11:27:48 +0100

    add zod schema for create schema

---
+++ lib/schemas/database/schema.ts
@@ -0,0 +1,9 @@
+import z from "zod";
+
+export const createSchemaSchema = z.object({
+    schemaName: z
+        .string()
+        .min(1, { message: "Schema name is required" })
+        .max(100, { message: "Schema name must be less than 100 characters" })
+        .regex(/^[a-zA-Z][a-zA-Z0-9_]*$/, { message: "Schema name can only contain letters, numbers, and underscores and starts with a letter" }),
+});
